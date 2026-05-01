0a5838030c7b772d52f9c63b1b253f06266ac5c5
Asim Abdulrahman Mohammed Omer <almhbob.iii@gmail.com>
2026-05-01 01:08:51 +0000

    Add mobile mock data

---
+++ apps/mobile/src/data/mock.ts
@@ -0,0 +1,9 @@
+export const drivers = [
+  { id: '1', name: 'Mohammed Ahmed', rating: 4.8, vehicle: 'Blue rickshaw', eta: '3 min' },
+  { id: '2', name: 'Ali Altayeb', rating: 4.7, vehicle: 'Yellow rickshaw', eta: '5 min' }
+];
+
+export const trips = [
+  { id: 't1', from: 'Market', to: 'Hospital', fare: '1200 SDG', date: 'Today' },
+  { id: 't2', from: 'Station', to: 'Residential', fare: '1500 SDG', date: 'Yesterday' }
+];
